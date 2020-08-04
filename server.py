68003ed3d72a744bac636c02e3c8267616d8db30
Samuel Addotey <samaddotey@yahoo.com>
2020-08-04 12:25:27 +0100

    adding templatees to fladk

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ Created on Fri Jul 31 23:51:54 2020
 import requests
 import flask
 from flask import request
+from flask import render_template
 from flask_cors import CORS, cross_origin
 import sys
 import pickle
@@ -20,7 +21,7 @@ loaded_model = pickle.load(open(filename, 'rb'))
 # dictionary to interpret solutions for output   
 solutions = {0: "SG G-Cash", 1:"SG MyHedge", 2:"SG Lyxor AP", 3:"SG Analyst", 4:"SG Coop", 5:"SG Clearing Solutions", 6:"SG D-View"}
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__,  template_folder='template')
 app.config["DEBUG"] = False 
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources=r'/algo', headers='Content-Type')
@@ -53,14 +54,10 @@ def index():
             })
 
 
-"""
-@app.route('/evaluate', methods = ["POST"])
+@app.route('/', methods = ["GET"])
+def loadPage():
+    return render_template("index.html")
 
-def predict():
-    data = request.get_json()
-    print(data, file=sys.stderr)
-    return flask.jsonify(data)
-"""
 
 if __name__ == "__main__":
     app.run(host = "127.0.0.1", port = 3001)
